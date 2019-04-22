69d71e3ee97fa5dc0f997ebafdccbc58eaa4ef14
jczuchnowski <jakub.czuchnowski@gmail.com>
2019-04-22 18:45:07 +0200

    refine Exceptions for Selector and SelectionKey

--- src/main/scala/scalaz/nio/channels/Selector.scala
+++ src/main/scala/scalaz/nio/channels/Selector.scala
@@ -1,43 +1,54 @@
 package scalaz.nio.channels
 
 import java.io.IOException
-import java.nio.channels.{ Selector => JSelector }
+import java.nio.channels.{ ClosedSelectorException, Selector => JSelector }
 
 import scalaz.nio.channels.spi.SelectorProvider
 import scalaz.nio.io._
-import scalaz.zio.{ IO, UIO }
+import scalaz.zio.{ IO, JustExceptions, UIO }
 import scalaz.zio.duration.Duration
 
 import scala.collection.JavaConverters
 
 class Selector(private[nio] val selector: JSelector) {
 
+  import Selector._
+
   final val isOpen: UIO[Boolean] = IO.effectTotal(selector.isOpen)
 
   final val provider: UIO[SelectorProvider] =
     IO.effectTotal(selector.provider()).map(new SelectorProvider(_))
 
-  final val keys: UIO[Set[SelectionKey]] =
-    IO.effectTotal(selector.keys()).map { keys =>
+  final val keys: IO[ClosedSelectorException, Set[SelectionKey]] =
+    IO.effect(selector.keys()).refineOrDie(JustClosedSelectorException).map { keys =>
       JavaConverters.asScalaSet(keys).toSet.map(new SelectionKey(_))
     }
 
-  final val selectedKeys: UIO[Set[SelectionKey]] =
-    IO.effectTotal(selector.selectedKeys()).map { keys =>
+  final val selectedKeys: IO[ClosedSelectorException, Set[SelectionKey]] =
+    IO.effect(selector.selectedKeys()).refineOrDie(JustClosedSelectorException).map { keys =>
       JavaConverters.asScalaSet(keys).toSet.map(new SelectionKey(_))
     }
 
   final def removeKey(key: SelectionKey): UIO[Unit] =
     IO.effectTotal(selector.selectedKeys().remove(key.selectionKey)).void
 
-  final val selectNow: IO[IOException, Int] =
-    IO.effect(selector.selectNow()).refineOrDie(JustIOException)
+  /**
+   * Can throw IOException and ClosedSelectorException.
+   */
+  final val selectNow: IO[Exception, Int] =
+    IO.effect(selector.selectNow()).refineOrDie(JustExceptions)
 
-  final def select(timeout: Duration): IO[IOException, Int] =
-    IO.effect(selector.select(timeout.toMillis)).refineOrDie(JustIOException)
+  /**
+   * Can throw IOException and ClosedSelectorException.
+   */
+  final def select(timeout: Duration): IO[Exception, Int] =
+    IO.effect(selector.select(timeout.toMillis)).refineOrDie(JustExceptions)
 
-  final val select: IO[IOException, Int] =
-    IO.effect(selector.select()).refineOrDie(JustIOException)
+  /**
+   * Can throw IOException and ClosedSelectorException.
+   */
+  final val select: IO[Exception, Int] =
+    IO.effect(selector.select()).refineOrDie(JustExceptions)
 
   final val wakeup: IO[Nothing, Selector] =
     IO.effectTotal(selector.wakeup()).map(new Selector(_))
@@ -47,6 +58,10 @@ class Selector(private[nio] val selector: JSelector) {
 }
 
 object Selector {
+
+  val JustClosedSelectorException: PartialFunction[Throwable, ClosedSelectorException] = {
+    case e: ClosedSelectorException => e
+  }
 
   final val make: IO[IOException, Selector] =
     IO.effect(new Selector(JSelector.open())).refineOrDie(JustIOException)
